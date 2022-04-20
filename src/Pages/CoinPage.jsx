617c3e7867deab6ae43535c79543bf96ae458695
Вадим Ильиных <vadimiiofficial@mail.ru>
2022-04-20 19:35:15 +0300

    problem with selectedNews 2

--- src/Pages/CoinPage.jsx
+++ src/Pages/CoinPage.jsx
@@ -6,36 +6,45 @@ import MyH from "../components/MyH/MyH";
 import Chart from "../components/Chart/Chart";
 import SelectButtons from "../components/SelectButtons/SelectButtons";
 import SearchInput from "../components/SearchInput/SearchInput";
-import NavigateBar from "../NavigateBar/NavigateBar";
+import {default_options} from "../components/Chart/options";
+import {useContext} from "react";
+import {MyContext} from "../components/Context/Context";
 
 const CoinPage = () => {
-    const [priceList, setPriceList] = useState([])
-    const [newsList, setNewsList] = useState([])
-    const [filteredNews, setFilteredNews] = useState([])
-    const [chartList, setChartList] = useState([])
-    const [coinName, setCoinName] = useState("")
-    const [searchStr, setSearchStr] = useState("")
-    const [selectedList, setSelectedList] = useState(1)
+    const [priceList, setPriceList] = useState([]);
+    const [newsList, setNewsList] = useState([]);
+    const [filteredNews, setFilteredNews] = useState([]);
+    const [chartList, setChartList] = useState([]);
+    const [coinName, setCoinName] = useState("");
+    const [searchStr, setSearchStr] = useState("");
+    const [selectedList, setSelectedList] = useState(1);
     const [limit, setLimit] = useState(50);
     const [pricePage, setPricePage] = useState(1);
     const [newsPage, setNewsPage] = useState(300);
+    const [newOptions, setNewOptions] = useState({});
+
+    const {selectedNews, setSelectedNews} = useContext(MyContext)
 
     useEffect(() => {
         async function fetchData() {
-            let coin_name = window.location.pathname.split(":")[1]
-            setCoinName(coin_name)
-            let new_coin_page = await MyRequest.getCoinPage(coin_name, pricePage, limit)
-            let chart_data = await MyRequest.getCoinHistory(coin_name)
-            let new_news_page = await MyRequest.getNewsPage(newsPage, limit)
+            let coin_name = window.location.pathname.split(":")[1];
+            setCoinName(coin_name);
+            let new_coin_page = await MyRequest.getCoinPage(coin_name, pricePage, limit);
+            let chart_data = await MyRequest.getCoinHistory(coin_name);
+            let new_news_page = await MyRequest.getNewsPage(newsPage, limit);
 
-            let coin_list = new_coin_page.history_list
-            setPriceList([...coin_list, ...priceList])
+            let coin_list = new_coin_page.history_list;
+            setPriceList([...coin_list, ...priceList]);
 
-            let chart_list = chart_data.history_list
-            setChartList([...chart_list])
+            let chart_list = chart_data.history_list;
+            setChartList([...chart_list]);
 
-            let news_list = new_news_page.news_list
-            setNewsList([...news_list, ...newsList])
+            let news_list = new_news_page.news_list;
+            setNewsList([...news_list, ...newsList]);
+
+            let options = {...default_options}
+            options.series[0].data = chart_list
+            setNewOptions({...options})
 
         }
         fetchData()
@@ -53,10 +62,35 @@ const CoinPage = () => {
         setFilteredNews(newsList.filter(news => news.news_title.includes(searchStr)))
     }, [searchStr])
 
+    useEffect(() => {
+        let options = {...newOptions}
+        console.log(options)
+        console.log(selectedNews)
+
+        if (selectedNews[0] !== undefined) {
+            let date_list = selectedNews[0].news_time.split("-")
+            options.series[1].data.push(
+                {
+                    x: Date.UTC(
+                        date_list[0],
+                        date_list[1],
+                        date_list[2],
+                        date_list[3],
+                        date_list[4],
+                        date_list[5]),
+                    title: selectedNews[0].news_title.slice(0, 10) + "...",
+                    text: selectedNews[0].news_title
+                })
+            console.log(options.series[1].data)
+            setNewOptions({...options})
+        }
+
+    }, [selectedNews])
+
     return (
         <div className="App">
             <MyH coin_name={coinName}/>
-            <Chart chart_list={chartList}/>
+            <Chart new_options={newOptions}/>
             <SelectButtons changeList={changeList}/>
             <SearchInput search_str={searchStr} set_search_str={setSearchStr}/>
             {selectedList
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,47 +2,15 @@ import React, {useEffect} from 'react';
 import Highcharts from 'highcharts/highstock'
 import HighchartsReact from 'highcharts-react-official';
 import classes from "./Chart.module.css";
-import {default_options} from "./options";
-import {useContext} from "react";
-import {MyContext} from "../Context/Context";
 
-const Chart = ({chart_list}) => {
-    const {selectedNews, setSelectedNews} = useContext(MyContext)
-    let options = {...default_options};
-    options.series[0].data = [...chart_list]
-
-    useEffect(() => {
-        console.log(selectedNews == false)
-        if (selectedNews != false)
-        {
-                console.log(selectedNews[0])
-                let date_list = selectedNews[0].news_time.split("-")
-                options.series[1].data.push(
-                    {
-                        x: Date.UTC(
-                            date_list[0],
-                            date_list[1],
-                            date_list[2],
-                            date_list[3],
-                            date_list[4],
-                            date_list[5]),
-                        title: selectedNews[0].news_title.slice(0, 10) + "...",
-                        text: selectedNews[0].news_title
-                    })
-                console.log(options.series[1].data)
-                console.log("...")
-                console.log(selectedNews)
-        }
-
-        }, [selectedNews]
-    )
+const Chart = ({new_options}) => {
 
     return (
         <div className={classes.myChart}>
             <HighchartsReact
                 highcharts={Highcharts}
                 constructorType={'stockChart'}
-                options={options}
+                options={new_options}
             />
         </div>
     );
